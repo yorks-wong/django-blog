319738200162830a5a8de740227cecd50844c05b
yorks-wong <1693958668@qq.com>
2018-12-04 16:18:34 +0800

    online demo

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7d&-f*rd$kbez(w-$ef2@nk9_fc9%*zjkgofhgl(=w*4lcs)*x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.yorks.wang']
 
 
 # Application definition
