bc2ac845e24d0179c3d6a44050a80484dfa5e8d8
yorks-wong <1693958668@qq.com>
2018-12-11 00:10:25 +0800

    优化

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7d&-f*rd$kbez(w-$ef2@nk9_fc9%*zjkgofhgl(=w*4lcs)*x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-DEBUG = True
 '''
+DEBUG = True
 ALLOWED_HOSTS = []
 '''
 DEBUG = False
